a28e76b1e1a1db47be9b620a47350766f5d62022
dhubmann <daniel.hubmann@gmx.at>
2023-08-08 08:11:56 +0200

    "Remove console.log() and reformat"
    Console.log() was called to check for the correct output of the todos.

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,12 @@ const App: React.FC = () => {
     }
   };
 
-  console.log(todos);
-
   return (
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-
-  <TodoList todos = {todos} setTodos = {setTodos}/>
-  
-  </div>
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
   );
 };
 
